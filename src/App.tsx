58f41463fd3e80bfaf67c010a61d179f4c35f386
Joseph Liao <100002111+JosephLiao542211@users.noreply.github.com>
2024-08-19 21:41:29 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,6 @@
 import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  const words = [
-    { text: "Vite", className: "text-[#a95eff]" },
-    { text: " + " },
-    { text: "React", className: "text-[#61dafb]" },
-    { text: " + " },
-    { text: "Tailwindcss", className: "text-[#0ea5e9]" },
-    { text: " + " },
-    { text: "Framer Motion", className: "text-[#ff57c8]" },
-  ];
 
   return (
     <div className="text-center">
