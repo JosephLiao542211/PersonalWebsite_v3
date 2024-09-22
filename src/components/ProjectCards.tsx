7ccbe52f0c397542d936640c1ebed67bcea2789f
JosephLiao542211 <billan452211@gmail.com>
2024-09-22 17:51:30 -0400

    fix

--- src/components/ProjectCards.tsx
+++ src/components/ProjectCards.tsx
@@ -12,12 +12,12 @@ const Card: React.FC<CardProps> = ({ title, link, description }) => {
       <div className="relative z-10 flex h-full justify-evenly overflow-hidden rounded-xl border-blue-500 bg-white bg-opacity-20 align-middle backdrop-blur-sm transition-all  md:hover:scale-105 md:hover:border-[7px] md:hover:border-blue-500">
         <div className="md:flex">
           <div className="md:w-2/5  ">
-            <img
-              src="image 7.jpg"
-              className=" object-cover sm:h-full md:rounded-s-xl"
-            >
-              {/* <a href={link}></a> */}
-            </img>
+            <a href={link}>
+              <img
+                src="image 7.jpg"
+                className=" object-cover sm:h-full md:rounded-s-xl"
+              ></img>
+            </a>
           </div>
           <div className="p-[5%] md:w-3/5">
             <div className="  font-bigger text-6xl uppercase tracking-wide text-white">
